3ce9939930ee86e8563b945f69cdfedc8610e8e9
Krittin SETDHAVANICH <krittin.set@ku.th>
2025-03-16 20:51:34 +0700

    chore: change the baseline to the yolov11l

--- simple_poc/main.py
+++ simple_poc/main.py
@@ -1,6 +1,6 @@
 from ui.app import MTMMCTrackerApp
 
 if __name__ == "__main__":
-    app = MTMMCTrackerApp(model_path="yolo11n.pt")
+    app = MTMMCTrackerApp(model_path="yolov11l.pt")
     demo = app.build_ui()
     demo.launch(share=True)
